7947438ebacca1aba86a5c81ae4b6f97f23dafe8
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-04-04 00:01:04 +0530

    Update __init__.py

--- ILovePDF/plugins/dm/action_inline/__init__.py
+++ ILovePDF/plugins/dm/action_inline/__init__.py
@@ -1,22 +1,20 @@
-# This module is part of https://github.com/nabilanavab/ilovepdf
-# Feel free to use and contribute to this project. Your contributions are welcome!
-# copyright ©️ 2021 nabilanavab
+
 
 file_name = "ILovePDF/plugins/dm/action_inline/__init__.py"
 
-__author__ = "nabilanavab"
-__email__ = "nabilanavab@gmail.com"
-__telegram__ = "telegram.dog/nabilanavab"
-__copyright__ = "Copyright 2021, nabilanavab"
+__author__ = "darkhacker34"
+__email__ = "nihalthoppil16@gmail.com"
+__telegram__ = "telegram.dog/nihh_all"
+__copyright__ = "Copyright 2024, darkhacker34"
 
 iLovePDF = '''
   _   _                  ___  ___  ____ ™
  | | | |   _____ _____  | _ \|   \|  __| 
  | | | |__/ _ \ V / -_) |  _/| |) |  _|  
  |_| |___,\___/\_/\___| |_|  |___/|_|    
-                         ❤ [Nabil A Navab] 
-                         ❤ Email: nabilanavab@gmail.com
-                         ❤ Telegram: @nabilanavab
+                         😎 [Nihal] 
+                         💌 Email: nihalthoppil16@gmail.com
+                         📞 Telegram: @nihh_all
 '''
 
 from plugins import *
