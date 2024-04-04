59c4654c0f34a4fe5624aa80402b4fe2ac67a5a9
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-04-04 08:06:00 +0530

    Update eng.py

--- ILovePDF/lang/eng.py
+++ ILovePDF/lang/eng.py
@@ -272,7 +272,7 @@ SETTINGS = {
     "result" : [_RES_FAIL, _RES_SUCCESS], "waitApi" : { _OPEN_BROW : "https://www.convertapi.com/a/signin" }, "error" : _ERROR_DB,
     "back" : [{ _BACK_HOME : "Home|B2S" }, { _BACK_HOME : "Home|B2A" }], "feedback" : _LANG_FEED, "ask" : [_SEND, _SEND_FAST],
     "thumb" : { _CREATOR : "https://t.me/nihh_all"},
-    "fname" : { _OWNER : "https://t.me/master_green_uae"}
+    "fname" : { _OWNER : "https://t.me/master_green_uae"},
     "api" : [{ _SET_API : "nabilanavab", _ADD : "set|api+", _BACK_HOME : "Home|B2S"},
              { _SET_API : "nabilanavab", _CHANGE : "set|api+", _DELETE : "set|api-", _BACK_HOME : "Home|B2S"}],
     "capt" : [{ _SET_CAPT : "nabilanavab", _ADD : "set|capt+", _BACK_HOME : "Home|B2S"},
