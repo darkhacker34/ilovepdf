b812009f6001133457b258e77ed6ea3d74d075b7
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-06-05 20:11:53 +0530

    Update __init__.py

--- ILovePDF/images/__init__.py
+++ ILovePDF/images/__init__.py
@@ -1,21 +1,19 @@
-# This module is part of https://github.com/nabilanavab/ilovepdf
-# Feel free to use and contribute to this project. Your contributions are welcome!
 
-file_name = "ILovePDF/images/__init__.py"
+file_name = "ILovePDF/configs/__init__.py"
 
-__author__ = "nabilanavab"
-__email__ = "nabilanavab@gmail.com"
-__telegram__ = "telegram.dog/nabilanavab"
-__copyright__ = "Copyright 2021, nabilanavab"
+__author__ = "darkhacker34"
+__email__ = "nihalthoppil16@gmail.com"
+__telegram__ = "telegram.dog/nihh_all"
+__copyright__ = "Copyright 2024, darkhacker34"
 
 iLovePDF = '''
   _   _                  ___  ___  ____ ™
  | | | |   _____ _____  | _ \|   \|  __| 
  | | | |__/ _ \ V / -_) |  _/| |) |  _|  
  |_| |___,\___/\_/\___| |_|  |___/|_|    
-                         ❤ [Nabil A Navab] 
-                         ❤ Email: nabilanavab@gmail.com
-                         ❤ Telegram: @nabilanavab
+                         😎 [NɪʜᴀL] 
+                         📩 Email: nihalthoppil16@gmail.com
+                         📞 Telegram: @nihh_all
 '''
 
 # If you have any questions or suggestions, please feel free to reach out.
