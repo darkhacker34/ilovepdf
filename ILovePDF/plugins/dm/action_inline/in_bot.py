70eefae9bd627554db2e6057d0d3b133085122ff
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-06-05 19:58:48 +0530

    Update in_bot.py

--- ILovePDF/plugins/dm/action_inline/in_bot.py
+++ ILovePDF/plugins/dm/action_inline/in_bot.py
@@ -17,7 +17,7 @@ async def download(current, total, bot, message):
                 [[
                     InlineKeyboardButton(
                         "📥 DOWNLOADED {:.2f}% 📥".format(current / total * 100),
-                        callback_data="https://t.me/ilovepdf_bot",)
+                        callback_data="http://t.me/Master_GreenBot",)
                 ]]
             )
         )
@@ -59,7 +59,7 @@ async def openInBot(bot, message, md5: Union[str, int]) -> bool:
         markup = InlineKeyboardMarkup(
             [[
                 InlineKeyboardButton(
-                    "⚠️ DOWNLOADING ⚠️", url="https://t.me/ilovepdf_bot")
+                    "⚠️ DOWNLOADING ⚠️", url="http://t.me/Master_GreenBot")
             ]]
         )
         data = await Libgen().search(
@@ -97,7 +97,7 @@ async def openInBot(bot, message, md5: Union[str, int]) -> bool:
             reply_markup=InlineKeyboardMarkup(
                 [[
                     InlineKeyboardButton(
-                        "🐍 STARTED UPLOADING 🐍", callback_data="https://t.me/ilovepdf_bot",)
+                        "🐍 STARTED UPLOADING 🐍", callback_data="http://t.me/Master_GreenBot",)
                 ]]
             )
         )
